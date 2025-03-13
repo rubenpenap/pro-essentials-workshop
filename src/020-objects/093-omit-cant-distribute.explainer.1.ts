5828f0528414c70f06cdeb65fd3639d7877c63da
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-13 18:04:20 -0300

    refactor: implement DistributiveOmit type for enhanced type manipulation

--- src/020-objects/093-omit-cant-distribute.explainer.1.ts
+++ src/020-objects/093-omit-cant-distribute.explainer.1.ts
@@ -1,48 +1,52 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 type User = {
-  id: string;
-  name: string;
-  age: number;
-  imageId: string;
+	id: string;
+	name: string;
+	age: number;
+	imageId: string;
 };
 
 type Organisation = {
-  id: string;
-  name: string;
-  address: string;
-  imageId: string;
+	id: string;
+	name: string;
+	address: string;
+	imageId: string;
 };
 
 type Product = {
-  id: string;
-  name: string;
-  price: number;
-  imageId: string;
+	id: string;
+	name: string;
+	price: number;
+	imageId: string;
 };
 
 type Entity = User | Organisation | Product;
 
-type EntityWithoutId = Omit<Entity, "id">;
+type DistributiveOmit<T, K extends PropertyKey> = T extends any
+	? Omit<T, K>
+	: never;
+
+type EntityWithoutId = DistributiveOmit<Entity, 'id'>;
 //   ^?
 
 type test = Expect<
-  Equal<
-    EntityWithoutId,
-    | {
-        name: string;
-        age: number;
-        imageId: string;
-      }
-    | {
-        name: string;
-        address: string;
-        imageId: string;
-      }
-    | {
-        name: string;
-        price: number;
-        imageId: string;
-      }
-  >
+	Equal<
+		EntityWithoutId,
+		| {
+				name: string;
+				age: number;
+				imageId: string;
+		  }
+		| {
+				name: string;
+				address: string;
+				imageId: string;
+		  }
+		| {
+				name: string;
+				price: number;
+				imageId: string;
+		  }
+	>
 >;
