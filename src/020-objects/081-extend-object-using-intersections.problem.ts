111a58eca20be1935b58e016818f00f045b55cdf
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-13 10:47:20 -0300

    refactor: extend User and Product types with BaseEntity properties

--- src/020-objects/081-extend-object-using-intersections.problem.ts
+++ src/020-objects/081-extend-object-using-intersections.problem.ts
@@ -1,40 +1,45 @@
-import { Extends, Expect } from "@total-typescript/helpers";
+import { Extends, Expect } from '@total-typescript/helpers';
+
+type BaseEntity = {
+	id: string;
+	createdAt: Date;
+};
 
 type User = {
-  id: string;
-  createdAt: Date;
-  name: string;
-  email: string;
-};
+	id: string;
+	createdAt: Date;
+	name: string;
+	email: string;
+} & BaseEntity;
 
 type Product = {
-  id: string;
-  createdAt: Date;
-  name: string;
-  price: number;
-};
+	id: string;
+	createdAt: Date;
+	name: string;
+	price: number;
+} & BaseEntity;
 
 type tests = [
-  Expect<
-    Extends<
-      {
-        id: string;
-        createdAt: Date;
-        name: string;
-        email: string;
-      },
-      User
-    >
-  >,
-  Expect<
-    Extends<
-      {
-        id: string;
-        createdAt: Date;
-        name: string;
-        price: number;
-      },
-      Product
-    >
-  >,
+	Expect<
+		Extends<
+			{
+				id: string;
+				createdAt: Date;
+				name: string;
+				email: string;
+			},
+			User
+		>
+	>,
+	Expect<
+		Extends<
+			{
+				id: string;
+				createdAt: Date;
+				name: string;
+				price: number;
+			},
+			Product
+		>
+	>,
 ];
