f4996203aceb80018eb069f376f36eaeeb9a5ff1
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-18 23:38:38 -0300

    Refactor: Add position getter to CanvasNode class and standardize the use of single quotes in the test cases.

--- src/030-classes/111-getters.problem.ts
+++ src/030-classes/111-getters.problem.ts
@@ -1,35 +1,42 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
 class CanvasNode {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 
-  constructor(position?: { x: number; y: number }) {
-    this.x = position?.x ?? 0;
-    this.y = position?.y ?? 0;
-  }
+	constructor(position?: { x: number; y: number }) {
+		this.x = position?.x ?? 0;
+		this.y = position?.y ?? 0;
+	}
 
-  move(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
+	get position() {
+		return {
+			x: this.x,
+			y: this.y,
+		};
+	}
+
+	move(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
 }
 
-it("Should be able to move", () => {
-  const canvasNode = new CanvasNode();
+it('Should be able to move', () => {
+	const canvasNode = new CanvasNode();
 
-  expect(canvasNode.position).toEqual({ x: 0, y: 0 });
+	expect(canvasNode.position).toEqual({ x: 0, y: 0 });
 
-  canvasNode.move(10, 20);
+	canvasNode.move(10, 20);
 
-  expect(canvasNode.position).toEqual({ x: 10, y: 20 });
+	expect(canvasNode.position).toEqual({ x: 10, y: 20 });
 });
 
-it("Should be able to receive an initial position", () => {
-  const canvasNode = new CanvasNode({
-    x: 10,
-    y: 20,
-  });
+it('Should be able to receive an initial position', () => {
+	const canvasNode = new CanvasNode({
+		x: 10,
+		y: 20,
+	});
 
-  expect(canvasNode.position).toEqual({ x: 10, y: 20 });
+	expect(canvasNode.position).toEqual({ x: 10, y: 20 });
 });
