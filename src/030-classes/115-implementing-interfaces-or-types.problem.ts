a73ded1fa27f8a7e826760de0d9357608344379d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-18 23:47:10 -0300

    Refactor: Implement IShape interface in Shape class and standardize the use of single quotes in the problems file.

--- src/030-classes/115-implementing-interfaces-or-types.problem.ts
+++ src/030-classes/115-implementing-interfaces-or-types.problem.ts
@@ -1,32 +1,31 @@
 interface ShapeOptions {
-  x: number;
-  y: number;
+	x: number;
+	y: number;
 }
 
 type IShape = {
-  position: { x: number; y: number };
-  move: (deltaX: number, deltaY: number) => void;
+	position: { x: number; y: number };
+	move: (deltaX: number, deltaY: number) => void;
 };
 
-// How do we ensure our Shape class matches IShape?
-class Shape {
-  #x: number;
-  #y: number;
+class Shape implements IShape {
+	#x: number;
+	#y: number;
 
-  constructor(initial?: ShapeOptions) {
-    this.#x = initial?.x ?? 0;
-    this.#y = initial?.y ?? 0;
-  }
+	constructor(initial?: ShapeOptions) {
+		this.#x = initial?.x ?? 0;
+		this.#y = initial?.y ?? 0;
+	}
 
-  get position() {
-    return {
-      x: this.#x,
-      y: this.#y,
-    };
-  }
+	get position() {
+		return {
+			x: this.#x,
+			y: this.#y,
+		};
+	}
 
-  move(x: number, y: number) {
-    this.#x = x;
-    this.#y = y;
-  }
+	move(x: number, y: number) {
+		this.#x = x;
+		this.#y = y;
+	}
 }
