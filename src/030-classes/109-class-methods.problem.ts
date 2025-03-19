288a58352c28f33b458d4f50ef1d75d691afc2b9
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-18 23:23:08 -0300

    Refactor: Add move method to CanvasNode class and standardize the use of single quotes in the test case.

--- src/030-classes/109-class-methods.problem.ts
+++ src/030-classes/109-class-methods.problem.ts
@@ -1,18 +1,23 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
 class CanvasNode {
-  x = 0;
-  y = 0;
+	x = 0;
+	y = 0;
+
+	move(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
 }
 
-it("Should be able to move", () => {
-  const canvasNode = new CanvasNode();
+it('Should be able to move', () => {
+	const canvasNode = new CanvasNode();
 
-  expect(canvasNode.x).toEqual(0);
-  expect(canvasNode.y).toEqual(0);
+	expect(canvasNode.x).toEqual(0);
+	expect(canvasNode.y).toEqual(0);
 
-  canvasNode.move(10, 20);
+	canvasNode.move(10, 20);
 
-  expect(canvasNode.x).toEqual(10);
-  expect(canvasNode.y).toEqual(20);
+	expect(canvasNode.x).toEqual(10);
+	expect(canvasNode.y).toEqual(20);
 });
