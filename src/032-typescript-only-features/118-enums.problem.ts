47ac1fe46213afbfe672889c41f36fff66564d59
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-20 20:55:44 -0300

    Refactor: Replace object-based LogLevel with enum and standardize the use of single quotes in the test cases.

--- src/032-typescript-only-features/118-enums.problem.ts
+++ src/032-typescript-only-features/118-enums.problem.ts
@@ -1,56 +1,60 @@
-import { expect, it, vi } from "vitest";
+import { expect, it, vi } from 'vitest';
 
-const LogLevel = {
-  DEBUG: 0,
-  INFO: 1,
-  WARN: 2,
-  ERROR: 3,
-};
-
-function log(opts: { globalLogLevel: number; level: number; message: string }) {
-  if (opts.level >= opts.globalLogLevel) {
-    console.log(opts.message);
-  }
+enum LogLevel {
+	DEBUG,
+	INFO,
+	WARN,
+	ERROR,
 }
 
-it("Should log if the level is higher than the global log level", () => {
-  const consoleLog = vi.spyOn(console, "log");
-  log({
-    globalLogLevel: LogLevel.INFO,
-    level: LogLevel.ERROR,
-    message: "Hello!",
-  });
+function log(opts: {
+	globalLogLevel: LogLevel;
+	level: LogLevel;
+	message: string;
+}) {
+	if (opts.level >= opts.globalLogLevel) {
+		console.log(opts.message);
+	}
+}
 
-  expect(consoleLog).toHaveBeenCalledWith("Hello!");
+it('Should log if the level is higher than the global log level', () => {
+	const consoleLog = vi.spyOn(console, 'log');
+	log({
+		globalLogLevel: LogLevel.INFO,
+		level: LogLevel.ERROR,
+		message: 'Hello!',
+	});
+
+	expect(consoleLog).toHaveBeenCalledWith('Hello!');
 });
 
-it("Should log if the level is equal to the global log level", () => {
-  const consoleLog = vi.spyOn(console, "log");
-  log({
-    globalLogLevel: LogLevel.INFO,
-    level: LogLevel.INFO,
-    message: "Hello!",
-  });
+it('Should log if the level is equal to the global log level', () => {
+	const consoleLog = vi.spyOn(console, 'log');
+	log({
+		globalLogLevel: LogLevel.INFO,
+		level: LogLevel.INFO,
+		message: 'Hello!',
+	});
 
-  expect(consoleLog).toHaveBeenCalledWith("Hello!");
+	expect(consoleLog).toHaveBeenCalledWith('Hello!');
 });
 
-it("Should not log if the level is lower than the global log level", () => {
-  const consoleLog = vi.spyOn(console, "log");
-  log({
-    globalLogLevel: LogLevel.INFO,
-    level: LogLevel.DEBUG,
-    message: "Hello!",
-  });
+it('Should not log if the level is lower than the global log level', () => {
+	const consoleLog = vi.spyOn(console, 'log');
+	log({
+		globalLogLevel: LogLevel.INFO,
+		level: LogLevel.DEBUG,
+		message: 'Hello!',
+	});
 
-  expect(consoleLog).not.toHaveBeenCalled();
+	expect(consoleLog).not.toHaveBeenCalled();
 });
 
-it("Should give you a TS error if you pass an invalid log level", () => {
-  log({
-    globalLogLevel: LogLevel.INFO,
-    // @ts-expect-error
-    level: 123,
-    message: "Hello!",
-  });
+it('Should give you a TS error if you pass an invalid log level', () => {
+	log({
+		globalLogLevel: LogLevel.INFO,
+		// @ts-expect-error
+		level: 123,
+		message: 'Hello!',
+	});
 });
