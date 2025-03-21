3315c30e440bafcf74abfdd828b594f859849c7a
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-20 22:04:54 -0300

    Refactor: Standardize the use of single quotes in the awaited type helper problem file and update User type definition to use Awaited for improved type safety.

--- src/040-deriving-types-from-values/134-awaited-type-helper.problem.ts
+++ src/040-deriving-types-from-values/134-awaited-type-helper.problem.ts
@@ -1,22 +1,22 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 const fetchUser = async (id: string) => {
-  return {
-    id,
-    name: "John Doe",
-    email: "example@email.com",
-  };
+	return {
+		id,
+		name: 'John Doe',
+		email: 'example@email.com',
+	};
 };
 
-type User = ReturnType<typeof fetchUser>;
+type User = Awaited<ReturnType<typeof fetchUser>>;
 
 type test = Expect<
-  Equal<
-    User,
-    {
-      id: string;
-      name: string;
-      email: string;
-    }
-  >
+	Equal<
+		User,
+		{
+			id: string;
+			name: string;
+			email: string;
+		}
+	>
 >;
