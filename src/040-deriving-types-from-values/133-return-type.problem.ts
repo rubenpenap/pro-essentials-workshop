d566039e16bc43b4c27958b26d94be9c3e999209
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-20 22:02:26 -0300

    Refactor: Standardize the use of single quotes in the return type problem file and update User type definition to use ReturnType for improved type safety.

--- src/040-deriving-types-from-values/133-return-type.problem.ts
+++ src/040-deriving-types-from-values/133-return-type.problem.ts
@@ -1,22 +1,22 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 const createUser = (id: string) => {
-  return {
-    id,
-    name: "John Doe",
-    email: "example@email.com",
-  };
+	return {
+		id,
+		name: 'John Doe',
+		email: 'example@email.com',
+	};
 };
 
-type User = unknown;
+type User = ReturnType<typeof createUser>;
 
 type test = Expect<
-  Equal<
-    User,
-    {
-      id: string;
-      name: string;
-      email: string;
-    }
-  >
+	Equal<
+		User,
+		{
+			id: string;
+			name: string;
+			email: string;
+		}
+	>
 >;
