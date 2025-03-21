d5f18cdf55b7a867a1c0bf533aafbc3b5b7484da
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-20 22:18:01 -0300

    Refactor: Standardize the use of single quotes in the programModes array and update AllPrograms type definition for improved clarity and type safety.

--- src/040-deriving-types-from-values/138-create-a-union-from-an-as-const-array.problem.ts
+++ src/040-deriving-types-from-values/138-create-a-union-from-an-as-const-array.problem.ts
@@ -1,24 +1,24 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 export const programModes = [
-  "group",
-  "announcement",
-  "1on1",
-  "selfDirected",
-  "planned1on1",
-  "plannedSelfDirected",
+	'group',
+	'announcement',
+	'1on1',
+	'selfDirected',
+	'planned1on1',
+	'plannedSelfDirected',
 ] as const;
 
-type AllPrograms = (typeof programModes)[keyof typeof programModes];
+type AllPrograms = (typeof programModes)[number];
 
 type test = Expect<
-  Equal<
-    AllPrograms,
-    | "group"
-    | "announcement"
-    | "1on1"
-    | "selfDirected"
-    | "planned1on1"
-    | "plannedSelfDirected"
-  >
+	Equal<
+		AllPrograms,
+		| 'group'
+		| 'announcement'
+		| '1on1'
+		| 'selfDirected'
+		| 'planned1on1'
+		| 'plannedSelfDirected'
+	>
 >;
