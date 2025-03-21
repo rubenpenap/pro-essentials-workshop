2e07e3141ab01ef5f776703ab34667223e6c9fed
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-20 22:14:15 -0300

    Refactor: Standardize the use of single quotes in indexed access type problem files and update type definitions for improved clarity and type safety.

--- src/040-deriving-types-from-values/136-pass-unions-to-indexed-access-types.problem.ts
+++ src/040-deriving-types-from-values/136-pass-unions-to-indexed-access-types.problem.ts
@@ -1,19 +1,20 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 export const programModeEnumMap = {
-  GROUP: "group",
-  ANNOUNCEMENT: "announcement",
-  ONE_ON_ONE: "1on1",
-  SELF_DIRECTED: "selfDirected",
-  PLANNED_ONE_ON_ONE: "planned1on1",
-  PLANNED_SELF_DIRECTED: "plannedSelfDirected",
+	GROUP: 'group',
+	ANNOUNCEMENT: 'announcement',
+	ONE_ON_ONE: '1on1',
+	SELF_DIRECTED: 'selfDirected',
+	PLANNED_ONE_ON_ONE: 'planned1on1',
+	PLANNED_SELF_DIRECTED: 'plannedSelfDirected',
 } as const;
 
 type ProgramModeMap = typeof programModeEnumMap;
 
-// How do we make sure PlannedPrograms stays in sync with programModeEnumMap?
-type PlannedPrograms = unknown;
+type PlannedPrograms = ProgramModeMap[
+	| 'PLANNED_ONE_ON_ONE'
+	| 'PLANNED_SELF_DIRECTED'];
 
 type test = Expect<
-  Equal<PlannedPrograms, "planned1on1" | "plannedSelfDirected">
+	Equal<PlannedPrograms, 'planned1on1' | 'plannedSelfDirected'>
 >;
