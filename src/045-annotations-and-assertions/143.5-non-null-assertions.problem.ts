3ffce56efa796526a1356d2cc521ffbc6670939d
Ruben Peña <reppdev@MacBook-Air-de-Angelica.local>
2025-03-22 12:46:32 -0300

    Refactor: Standardize the use of double quotes in the non-null assertions problem file and enhance type safety with non-null assertion operator for improved clarity.

--- src/045-annotations-and-assertions/143.5-non-null-assertions.problem.ts
+++ src/045-annotations-and-assertions/143.5-non-null-assertions.problem.ts
@@ -1,43 +1,43 @@
-import { Equal, Expect } from '@total-typescript/helpers';
-import { expect, it } from 'vitest';
+import { Equal, Expect } from "@total-typescript/helpers";
+import { expect, it } from "vitest";
 
 const findUsersByName = (
-	searchParams: { name?: string },
-	users: {
-		id: string;
-		name: string;
-	}[],
+  searchParams: { name?: string },
+  users: {
+    id: string;
+    name: string;
+  }[],
 ) => {
-	if (searchParams.name) {
-		return users.filter((user) => user.name.includes(searchParams.name!));
-	}
+  if (searchParams.name) {
+    return users.filter((user) => user.name.includes(searchParams.name!));
+  }
 
-	return users;
+  return users;
 };
 
-it('Should find the exact name', () => {
-	const result = findUsersByName(
-		{
-			name: 'Bob',
-		},
-		[
-			{
-				id: '1',
-				name: 'Bob',
-			},
-			{
-				id: '2',
-				name: 'Alice',
-			},
-		],
-	);
+it("Should find the exact name", () => {
+  const result = findUsersByName(
+    {
+      name: "Bob",
+    },
+    [
+      {
+        id: "1",
+        name: "Bob",
+      },
+      {
+        id: "2",
+        name: "Alice",
+      },
+    ],
+  );
 
-	expect(result).toEqual([
-		{
-			id: '1',
-			name: 'Bob',
-		},
-	]);
+  expect(result).toEqual([
+    {
+      id: "1",
+      name: "Bob",
+    },
+  ]);
 
-	type test = Expect<Equal<typeof result, { id: string; name: string }[]>>;
+  type test = Expect<Equal<typeof result, { id: string; name: string }[]>>;
 });
