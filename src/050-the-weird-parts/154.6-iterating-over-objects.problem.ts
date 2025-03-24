5013bf105d4f1a45c8ff6202f3af2b8d5e8ddfc2
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-23 21:20:04 -0300

    Refactor: Improve the printUser function to log user properties dynamically using Object.keys for enhanced flexibility and clarity in the iterating over objects problem file.

--- src/050-the-weird-parts/154.6-iterating-over-objects.problem.ts
+++ src/050-the-weird-parts/154.6-iterating-over-objects.problem.ts
@@ -1,20 +1,24 @@
-import { expect, it, vitest } from "vitest";
+import { expect, it, vitest } from 'vitest';
 
 interface User {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 }
 
-function printUser(user: User) {}
+function printUser(user: User) {
+	Object.keys(user).forEach((key) => {
+		console.log(user[key as keyof User]);
+	});
+}
 
-it("Should log all the keys of the user", () => {
-  const consoleSpy = vitest.spyOn(console, "log");
+it('Should log all the keys of the user', () => {
+	const consoleSpy = vitest.spyOn(console, 'log');
 
-  printUser({
-    id: 1,
-    name: "Waqas",
-  });
+	printUser({
+		id: 1,
+		name: 'Waqas',
+	});
 
-  expect(consoleSpy).toHaveBeenCalledWith(1);
-  expect(consoleSpy).toHaveBeenCalledWith("Waqas");
+	expect(consoleSpy).toHaveBeenCalledWith(1);
+	expect(consoleSpy).toHaveBeenCalledWith('Waqas');
 });
