53caa5cdab970fea421ac0ee5ebc6c9f088a31ee
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-23 21:21:44 -0300

    Refactor: Update selectedId initialization and type assignment for improved clarity and consistency in the evolving any problem file.

--- src/050-the-weird-parts/154.8-evolving-any.problem.ts
+++ src/050-the-weird-parts/154.8-evolving-any.problem.ts
@@ -1,9 +1,11 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
-let selectedId = 123;
+let selectedId;
+
+selectedId = 123;
 
 type test = Expect<Equal<typeof selectedId, number>>;
 
-selectedId = "123";
+selectedId = '123';
 
 type test2 = Expect<Equal<typeof selectedId, string>>;
