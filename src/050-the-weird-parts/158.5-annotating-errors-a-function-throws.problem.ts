acfd7d69e200d08a2a2e7e6fa8c0e403799dccb1
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-23 21:24:14 -0300

    Refactor: Enhance error handling and return type structure in getUserFromLocalStorage function for improved clarity and type safety in the error annotation problem file.

--- src/050-the-weird-parts/158.5-annotating-errors-a-function-throws.problem.ts
+++ src/050-the-weird-parts/158.5-annotating-errors-a-function-throws.problem.ts
@@ -1,21 +1,48 @@
-/**
- * How do we annotate the errors this function throws?
- */
+const getUserFromLocalStorage = (
+	id: string,
+):
+	| {
+			success: true;
+			data: any;
+	  }
+	| {
+			success: false;
+			error: SyntaxError | DOMException;
+	  } => {
+	try {
+		const user = localStorage.getItem(id);
+		if (!user) {
+			return {
+				success: true,
+				data: undefined,
+			};
+		}
 
-type PossibleErrors = SyntaxError | DOMException;
-
-const getUserFromLocalStorage = (id: string) => {
-  const user = localStorage.getItem(id);
-  if (!user) {
-    return undefined;
-  }
-
-  return JSON.parse(user);
+		return {
+			success: true,
+			data: JSON.parse(user),
+		};
+	} catch (e) {
+		if (e instanceof DOMException) {
+			return {
+				success: false,
+				error: e,
+			};
+		}
+		if (e instanceof SyntaxError) {
+			return {
+				success: false,
+				error: e,
+			};
+		}
+		throw e;
+	}
 };
 
-try {
-  const user = getUserFromLocalStorage("user-1");
-} catch (
-  // How do we make this typed as PossibleErrors?
-  e
-) {}
+const user = getUserFromLocalStorage('user-1');
+
+if (user.success) {
+	user.data;
+} else {
+	user.error;
+}
