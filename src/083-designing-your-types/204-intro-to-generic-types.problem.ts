330d2578b73778ffafd6843e814565c1cec16e3d
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-26 21:21:18 -0300

    Refactor: Simplify UserDataShape and PostDataShape types by introducing a generic DataShape type, enhancing type reusability and clarity in the generic types problem file.

--- src/083-designing-your-types/204-intro-to-generic-types.problem.ts
+++ src/083-designing-your-types/204-intro-to-generic-types.problem.ts
@@ -1,66 +1,64 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 type ErrorShape = {
-  error: {
-    message: string;
-  };
+	error: {
+		message: string;
+	};
 };
 
-type UserDataShape =
-  | {
-      data: {
-        id: string;
-        name: string;
-        email: string;
-      };
-    }
-  | ErrorShape;
+type DataShape<TData> =
+	| {
+			data: TData;
+	  }
+	| ErrorShape;
 
-type PostDataShape =
-  | {
-      data: {
-        id: string;
-        title: string;
-        body: string;
-      };
-    }
-  | ErrorShape;
+type UserDataShape = DataShape<{
+	id: string;
+	name: string;
+	email: string;
+}>;
+
+type PostDataShape = DataShape<{
+	id: string;
+	title: string;
+	body: string;
+}>;
 
 // TESTS
 
 type tests = [
-  Expect<
-    Equal<
-      UserDataShape,
-      | {
-          data: {
-            id: string;
-            name: string;
-            email: string;
-          };
-        }
-      | {
-          error: {
-            message: string;
-          };
-        }
-    >
-  >,
-  Expect<
-    Equal<
-      PostDataShape,
-      | {
-          data: {
-            id: string;
-            title: string;
-            body: string;
-          };
-        }
-      | {
-          error: {
-            message: string;
-          };
-        }
-    >
-  >,
+	Expect<
+		Equal<
+			UserDataShape,
+			| {
+					data: {
+						id: string;
+						name: string;
+						email: string;
+					};
+			  }
+			| {
+					error: {
+						message: string;
+					};
+			  }
+		>
+	>,
+	Expect<
+		Equal<
+			PostDataShape,
+			| {
+					data: {
+						id: string;
+						title: string;
+						body: string;
+					};
+			  }
+			| {
+					error: {
+						message: string;
+					};
+			  }
+		>
+	>,
 ];
