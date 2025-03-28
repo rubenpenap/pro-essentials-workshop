c3bb96bcafdeefe790d6997724e15e010db20039
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-27 22:15:59 -0300

    Refactor: Update AttributeGetters type to use mapped types with template literal keys for enhanced type safety in the mapped types problem file.

--- src/083-designing-your-types/213-as-in-mapped-types.problem.ts
+++ src/083-designing-your-types/213-as-in-mapped-types.problem.ts
@@ -1,24 +1,24 @@
-import { Equal, Expect } from "@total-typescript/helpers";
+import { Equal, Expect } from '@total-typescript/helpers';
 
 interface Attributes {
-  firstName: string;
-  lastName: string;
-  age: number;
+	firstName: string;
+	lastName: string;
+	age: number;
 }
 
 type AttributeGetters = {
-  [K in keyof Attributes]: () => Attributes[K];
+	[K in keyof Attributes as `get${Capitalize<K>}`]: () => Attributes[K];
 };
 
 type tests = [
-  Expect<
-    Equal<
-      AttributeGetters,
-      {
-        getFirstName: () => string;
-        getLastName: () => string;
-        getAge: () => number;
-      }
-    >
-  >,
+	Expect<
+		Equal<
+			AttributeGetters,
+			{
+				getFirstName: () => string;
+				getLastName: () => string;
+				getAge: () => number;
+			}
+		>
+	>,
 ];
