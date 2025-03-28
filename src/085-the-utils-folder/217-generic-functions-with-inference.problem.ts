e761a6b185f2e29d7df99ef274da7181ba33696f
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-27 22:17:22 -0300

    Refactor: Update uniqueArray function to use generics for improved type safety in the generic functions with inference problem file.

--- src/085-the-utils-folder/217-generic-functions-with-inference.problem.ts
+++ src/085-the-utils-folder/217-generic-functions-with-inference.problem.ts
@@ -1,22 +1,22 @@
-import { Equal, Expect } from "@total-typescript/helpers";
-import { expect, it } from "vitest";
+import { Equal, Expect } from '@total-typescript/helpers';
+import { expect, it } from 'vitest';
 
-const uniqueArray = (arr: any[]) => {
-  return Array.from(new Set(arr));
+const uniqueArray = <T>(arr: T[]) => {
+	return Array.from(new Set(arr));
 };
 
-it("returns an array of unique values", () => {
-  const result = uniqueArray([1, 1, 2, 3, 4, 4, 5]);
+it('returns an array of unique values', () => {
+	const result = uniqueArray([1, 1, 2, 3, 4, 4, 5]);
 
-  type test = Expect<Equal<typeof result, number[]>>;
+	type test = Expect<Equal<typeof result, number[]>>;
 
-  expect(result).toEqual([1, 2, 3, 4, 5]);
+	expect(result).toEqual([1, 2, 3, 4, 5]);
 });
 
-it("should work on strings", () => {
-  const result = uniqueArray(["a", "b", "b", "c", "c", "c"]);
+it('should work on strings', () => {
+	const result = uniqueArray(['a', 'b', 'b', 'c', 'c', 'c']);
 
-  type test = Expect<Equal<typeof result, string[]>>;
+	type test = Expect<Equal<typeof result, string[]>>;
 
-  expect(result).toEqual(["a", "b", "c"]);
+	expect(result).toEqual(['a', 'b', 'c']);
 });
